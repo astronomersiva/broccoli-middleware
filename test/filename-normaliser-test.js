49aca1f503b6d3a35a075613a74dd12112bb7fe2
Sivasubramanyam A <ssdragonar5@gmail.com>
2018-08-09 22:13:18 +0530

    Make tests pass on Windows

--- test/filename-normaliser-test.js
+++ test/filename-normaliser-test.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const expect = require('chai').expect;
+const path = require('path');
 const normaliseFileName = require('./../lib/utils/filename-normaliser').normaliseFileName;
 
 describe('file name normaliser', function() {
   describe('handles module unification layout', function() {
     it('components are normalised correctly', function() {
-      const fileName = 'src/ui/components/my-component/template.hbs';
+      const fileName = path.normalize('src/ui/components/my-component/template.hbs');
       const normalisedFileName = normaliseFileName(fileName);
 
       expect(normalisedFileName).to.equal(fileName);
@@ -15,16 +16,16 @@ describe('file name normaliser', function() {
 
   describe('handles pod layout', function() {
     it('components are normalised correctly', function() {
-      const fileName = 'broccoli-middleware/components/awesome-component/template.hbs';
-      const expectedFileName = 'app/components/awesome-component/template.hbs';
+      const fileName = path.normalize('broccoli-middleware/components/awesome-component/template.hbs');
+      const expectedFileName = path.normalize('app/components/awesome-component/template.hbs');
       const normalisedFileName = normaliseFileName(fileName);
 
       expect(normalisedFileName).to.equal(expectedFileName);
     });
 
     it('templates are normalised correctly', function() {
-      const fileName = 'broccoli-middleware/awesome-route/template.hbs';
-      const expectedFileName = 'app/awesome-route/template.hbs';
+      const fileName = path.normalize('broccoli-middleware/awesome-route/template.hbs');
+      const expectedFileName = path.normalize('app/awesome-route/template.hbs');
       const normalisedFileName = normaliseFileName(fileName);
 
       expect(normalisedFileName).to.equal(expectedFileName);
@@ -32,16 +33,16 @@ describe('file name normaliser', function() {
 
     describe('in-repo engines are normalised correctly', function() {
       it('components are normalised correctly', function() {
-        const fileName = 'awesome-engine/components/awesome-component/template.hbs';
-        const expectedFileName = 'lib/awesome-engine/addon/components/awesome-component/template.hbs';
+        const fileName = path.normalize('awesome-engine/components/awesome-component/template.hbs');
+        const expectedFileName = path.normalize('lib/awesome-engine/addon/components/awesome-component/template.hbs');
         const normalisedFileName = normaliseFileName(fileName);
 
         expect(normalisedFileName).to.equal(expectedFileName);
       });
 
       it('templates are normalised correctly', function() {
-        const fileName = 'awesome-engine/awesome-route/template.hbs';
-        const expectedFileName = 'lib/awesome-engine/addon/awesome-route/template.hbs';
+        const fileName = path.normalize('awesome-engine/awesome-route/template.hbs');
+        const expectedFileName = path.normalize('lib/awesome-engine/addon/awesome-route/template.hbs');
         const normalisedFileName = normaliseFileName(fileName);
 
         expect(normalisedFileName).to.equal(expectedFileName);
@@ -51,16 +52,16 @@ describe('file name normaliser', function() {
 
   describe('handles classic layout', function() {
     it('components are normalised correctly', function() {
-      const fileName = 'broccoli-middleware/templates/components/awesome-component.hbs';
-      const expectedFileName = 'app/templates/components/awesome-component.hbs';
+      const fileName = path.normalize('broccoli-middleware/templates/components/awesome-component.hbs');
+      const expectedFileName = path.normalize('app/templates/components/awesome-component.hbs');
       const normalisedFileName = normaliseFileName(fileName);
 
       expect(normalisedFileName).to.equal(expectedFileName);
     });
 
     it('templates are normalised correctly', function() {
-      const fileName = 'broccoli-middleware/templates/application.hbs';
-      const expectedFileName = 'app/templates/application.hbs';
+      const fileName = path.normalize('broccoli-middleware/templates/application.hbs');
+      const expectedFileName = path.normalize('app/templates/application.hbs');
       const normalisedFileName = normaliseFileName(fileName);
 
       expect(normalisedFileName).to.equal(expectedFileName);
@@ -68,16 +69,16 @@ describe('file name normaliser', function() {
 
     describe('in-repo engines are normalised correctly', function() {
       it('components are normalised correctly', function() {
-        const fileName = 'awesome-engine/templates/components/awesome-component.hbs';
-        const expectedFileName = 'lib/awesome-engine/addon/templates/components/awesome-component.hbs';
+        const fileName = path.normalize('awesome-engine/templates/components/awesome-component.hbs');
+        const expectedFileName = path.normalize('lib/awesome-engine/addon/templates/components/awesome-component.hbs');
         const normalisedFileName = normaliseFileName(fileName);
 
         expect(normalisedFileName).to.equal(expectedFileName);
       });
 
       it('templates are normalised correctly', function() {
-        const fileName = 'awesome-engine/templates/awesome-route.hbs';
-        const expectedFileName = 'lib/awesome-engine/addon/templates/awesome-route.hbs';
+        const fileName = path.normalize('awesome-engine/templates/awesome-route.hbs');
+        const expectedFileName = path.normalize('lib/awesome-engine/addon/templates/awesome-route.hbs');
         const normalisedFileName = normaliseFileName(fileName);
 
         expect(normalisedFileName).to.equal(expectedFileName);
